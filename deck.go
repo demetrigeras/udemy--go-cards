f8555c893df07c58388b75b65a65636ad07a7354
demetrigeras <demetrijgeras@gmail.com>
2023-11-15 09:42:01 -0500

    cards udemy

--- deck.go
+++ deck.go
@@ -4,6 +4,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	CardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for i, suit := range CardSuits {
+		for j, value := range CardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
